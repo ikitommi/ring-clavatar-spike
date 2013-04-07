54284af63a22055027916561f007bd465df34841
Tommi Reiman <tommi.reiman@metosin.fi>
2013-04-07 14:57:52 +0300

    change name

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject gravatar "0.1.0-SNAPSHOT"
-  :description "Gravatar test"
+(defproject ring-clavatar-spike "0.1.0-SNAPSHOT"
+  :description "Ring Clavatar Spike"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.3"]
