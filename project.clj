eaa443163cd75dcb6053c90f50fb81fcc1dcb316
Tommi Reiman <tommi.reiman@metosin.fi>
2013-04-07 14:49:32 +0300

    updated deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.3"]
-                 [midje "1.5.1"]
                  [clavatar "0.2.1"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler gravatar.handler/app}
-  :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+  :profiles {:dev {:dependencies [[ring-mock "0.1.3"]
+                                  [midje "1.5.1"]]
+                   :plugins [[lein-midje "2.0.1"]]}})
